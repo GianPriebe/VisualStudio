a11a02c7081a6a9a0f1e3fb7e0a1f2f3dab358fa
unknown <VISITANTE7@asav.brm>
2019-12-10 17:31:50 -0300

    commit on master

--- src/main/java/com/example/ProductResource.java
+++ src/main/java/com/example/ProductResource.java
@@ -3,6 +3,7 @@ package com.example;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+<<<<<<< HEAD
 
 @RestController
 public class ProductResource {
@@ -21,3 +22,23 @@ public class ProductResource {
   }
 
 }
+=======
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+public class ProductResource {
+    private Product produto;
+
+    public ProductResource(){
+        this.produto = new Product();
+        produto.setValor(25.00);
+}
+@RequestMapping(value="/produtos", method=RequestMethod.GET)
+public Product buscarProdutos() {
+   return produto;
+   //return "Greetings from Spring Boot!";
+    }
+
+}
+>>>>>>> commit on master
